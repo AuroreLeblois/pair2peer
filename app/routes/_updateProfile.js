c88a092a5ecd700187246b8b526a80efad6a4681
AuroreLeblois <leblois-aurore@hotmail.fr>
2020-04-30 13:16:30 +0200

    patch almost ok but missing delete option

--- app/routes/_updateProfile.js
+++ app/routes/_updateProfile.js
@@ -63,7 +63,8 @@ module.exports = {
                 //on retrouve l'user de suite pour ne pas avoir à le refaire plus tard
                 const result = await db.query(`SELECT * FROM usr WHERE email = $1`, [email]);
                 const userID= result.rows[0].id;
-                //les données du formulaire
+                //les données du formulaire:
+                //les données utilisateur brutes
                 let password= request.payload.password;
                 let birthyear= request.payload.birthyear;
                 let picture= request.payload.picture;
@@ -74,12 +75,22 @@ module.exports = {
                 let description= request.payload.description;
                 let validatePassword= request.payload.validatePassword;
                 let changeMyEmail= request.payload.email;
+                let validateEmail= request.payload.validateEmail;
                 let pseudo= request.payload.pseudo;
+                //les langues
                 let selectedLang= [request.payload.languages];
+                //les it
                 let itlangs = [request.payload.itLanguages];
                 let itLevel= [request.payload.itLanguages.itLevels];
+                //on peut me trouver via le filtre?
                 let searchMe= request.payload.searchable;
+                //les dispo
+                let days= [request.payload.disponibilityDays];
+                let startSession= request.payload.hourSessionStart;
+                let stopSession= request.payload.hourSessionStop;
                 
+                //petit console.log(request.payload) pour vérifier tout ça
+                console.log(request.payload);
                 //si l'utisateur change des infos=> update user table
                 //le speudo
                 if(pseudo!== undefined||pseudo!== null||pseudo.length>0){
@@ -97,10 +108,13 @@ module.exports = {
                 //on compare le mdp avec la validation si mdp changé
                 //déjà est ce que le user a rentré un mdp?
                 if(password!== null||password!== undefined||password.length>0){
+                    console.log('un mot de passe a été saisie!')
                     //est ce que le mdp fait bien 8 caractères au moins?
                     if(password.length>=8){
+                        console.log('le mdp est supérieur à 8 caractères')
                         //est ce que le mdp ===  validation?
                         if(password===validatePassword){
+                            console.log('le mdp est le meme que la validation')
                             await db.query(`UPDATE usr
                                             SET "password"= ${password} 
                                             WHERE usr.id=${userID}`);
@@ -113,7 +127,8 @@ module.exports = {
                 };
                 //l'email
                 //est-ce que le champs email est rempli et différent du cookie?
-                if(changeMyEmail!== null||changeMyEmail!== undefined||changeMyEmail.length>0||changeMyEmail!==email){
+                if(changeMyEmail!== null||changeMyEmail!== undefined||changeMyEmail.length>0||changeMyEmail!==email&& changeMyEmail===validateEmail){
+                    console.log('un email a été saisie et est différent+validation ok')
                     //ok mais c'est un email?
                     if(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(changeMyEmail)===true){
                     //dans ce cas on va update le profil
@@ -123,7 +138,7 @@ module.exports = {
                     }
                     //sinon on prévient l'user
                     else{
-                        error.push(`L'email saisie est invalide.`)
+                        error.push(`L'email saisie est invalide ou n'est pas équivalent à la validation.`)
                     }
                 };
                 
@@ -170,7 +185,7 @@ module.exports = {
                     for (let lang of selectedLang){
                         const langID= await db.query(`SELECT id 
                                                       FROM lang
-                                                      WHERE "name" LIKE %$1`, [lang]);
+                                                      WHERE "name" LIKE $1`, [lang]);
     
                         const userKnowsLang= await db.query(`SELECT usr_id, lang_id 
                                                             FROM usr_speaks_lang
@@ -207,20 +222,58 @@ module.exports = {
                             AND it_lang_id ${itLangID}`);
                         }
                     }
-                }
-                //si il y a des erreurs
-                if(error.length>0){
-                    return error
-                }
-                else{
-                    const user= result.rows[0];
-                    return  user;
-                }
-                //si le champs est vide=> ne rien faire
+                    };
                 
-         
-            }
-        });
+                //diponibility
+                //est ce que j'ai sélectionné des jours?
+                if(days.length>0){
+                    //la dispo existe?
+                    for(let day of days){
+                        
+                   const dispoExists= await db.query(`SELECT * 
+                                                    FROM disponibility
+                                                    WHERE usr_id=${userID}
+                                                    AND day=${day}`);
+                    //non
+                    if(!dispoExists.rows[0]){
+                        //pour chaque jour=>insert du jour pour créer la ligne
+                            await db.query(`INSERT INTO disponibility(day, usr_id)
+                                            VALUES (${day}, ${userID})`)
+                         
+                         if(startSession!==null||startSession!==undefined||startSession!==isNaN(startSession)){
+                            //l'user a rentré une fin?
+                                if(stopSession!==null||stopSession!==undefined||stopSession!==isNaN(stopSession)){
+                                    let interval= stopSession-startSession;
+                                    const intervalSession= interval+'H';
+                                }
+                            }
+                        }
+                    }
+                    //et si aucune case n'est coché=> user ne veut plus faire de session donc... plus dispo
+                }else{
+                    const dispoExists= await db.query(`SELECT * 
+                                                    FROM disponibility
+                                                    WHERE usr_id=${userID}
+                                                    `);
+                    if(dispoExists){
+                        await db.query(`DELETE * 
+                                        FROM disponibility
+                                        WHERE usr_id=${userID}`)
+                    }
+                }
+                
 
-    }
-}
+                        //si il y a des erreurs
+                        if(error.length>0){
+                            return error
+                        }
+                        else{
+                            const user= result.rows[0];
+                            return  user;
+                        }
+                        //si le champs est vide=> ne rien faire
+                    }
+                });
+        
+            }
+        }
