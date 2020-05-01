c3da1282d10b13445df252036323c303f29ea047
AuroreLeblois <leblois-aurore@hotmail.fr>
2020-05-01 14:01:38 +0200

    sorry serail!=serial

--- app/migrations/deploy/domaines.sql
+++ app/migrations/deploy/domaines.sql
@@ -5,6 +5,6 @@ BEGIN;
 CREATE DOMAIN chat_room_serial AS text
 CHECK (VALUE ~* '^[0-9a-f]{6}$');
 
-ALTER TABLE chat ALTER chat_serail TYPE chat_room_serial;
+ALTER TABLE chat ALTER chat_serial TYPE chat_room_serial;
 
 COMMIT;
