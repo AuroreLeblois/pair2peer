9ec69b01b749db11b1ca924f70019e4639c4cdef
xooma <bec.maximilien@gmail.com>
2020-05-01 09:58:11 +0200

    Update login form

--- web/src/store/utils.js
+++ web/src/store/utils.js
@@ -3,7 +3,6 @@ export const getFiltersData = (data) => {
   const filters = {};
   filters.it_language = data.it_language.name;
   filters.language = data.language.name;
-  filters.country = data.localisation.map((place) => place.country);
-  filters.city = data.localisation.map((place) => place.city);
+  filters.localisation = data.localisation
   return filters;
 };
