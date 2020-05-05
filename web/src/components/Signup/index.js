c5b546200198ad54b6130c59bcf34726651fb07a
Greiveldinger Victor <victorgreiveldinger@gmail.com>
2020-05-05 10:12:22 +0200

    correction

--- web/src/components/Signup/index.js
+++ web/src/components/Signup/index.js
@@ -98,52 +98,76 @@ const Signup = () => {
           pseudo:'',
           email: '',
           password:'',
-          ville:'',
-          pays:'',
-          validationPassword:'',
+          passwordConfirm:'',
+          country:'',
+          city:'',
           remote:'',
-          acceptedTerms: false, // Pour la checkbox
+          role:'defaut ??',
+          // acceptedTerms: false, // Pour la checkbox
         }
       }
+
+      // Gerer coté back mais possibilité de rajout en front ??
       validationSchema = {
         Yup.object (
           {
             pseudo: Yup.string()
             .max(15, 'Must be 15 characters or less')
-            .required('Required'),
-            password: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('Required'),
-            validationPassword: Yup.string()
-            .max(20, 'Must be 20 characters or less')
-            .required('Required'),
+            .required('Obligatoire'),
+            country: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Obligatoire'),
+            city: Yup.string()
+            .max(15, 'Must be 15 characters or less')
+            .required('Obligatoire'),
             email: Yup.string()
             .email('Invalid email address')
-            .required('Required'),
+            .required('Obligatoire'),
             remote: Yup.boolean()
-            .required('Required')
+            .required('Obligatoire')
             .oneOf([true], 'You must accept the terms and conditions.'),
             //acceptedTerms: Yup.boolean()
             //.required('Required')
             //.oneOf([true], 'You must accept the terms and conditions.'), // Pour une future Charte de bonne conduite par exemple
           }
-        )
+        ).shape({
+          password: Yup.string().required("Obligatoire"),
+          passwordConfirm: Yup.string().when("password", {
+            is: val => (val && val.length > 0 ? true : false),
+            then: Yup.string().oneOf(
+              [Yup.ref("password")],
+              "Both password need to be the same"
+            )
+          })
+        })
       }
       onSubmit = {
         (values, { setSubmitting } ) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
-          }, 0
+          }, 400
           );
       }}
+      // Possibilité de faire un forEach de l'objet Yup pour construire chaque form ??
     >
+      
       <Form className = "ui form">
           <MyTextInput
             label="Pseudo"
             name="pseudo"
             type="text"
             placeholder="Jane57"
+          />
+          <MyTextInput
+            label="Mail"
+            name="email"
+            error={{
+              content: 'Une adresse mail valide pardi !',
+              pointing: 'below',
+            }}
+            type="email"
+            placeholder="jane@formik.com"
           />
           <MyTextInput
             label="Mot de passe"
@@ -153,33 +177,26 @@ const Signup = () => {
           />
           <MyTextInput
             label="Confirmation de mot de passe"
-            name="password"
+            name="passwordConfirm"
             type="password"
             placeholder=""
           />
           <MyTextInput
-            label="Mail"
-            name="email"
-            type="email"
-            placeholder="jane@formik.com"
-          />
-          <MyTextInput
             label="Pays"
-            name="pays"
+            name="country"
             type="text"
             placeholder="France"
           />
           <MyTextInput
           label="Ville"
-          name="ville"
+          name="city"
           type="text"
           placeholder="Strasbourg"
         />
           <MyCheckbox
             name="remote"
             type="checkbox"            
-          >
-            Voulez-vous être en remote ?
+          >Voulez-vous travailler en remote ?
           </MyCheckbox>
           {/* <MyCheckbox name="acceptedTerms">
             J'accepte les conditions d'utilisations
