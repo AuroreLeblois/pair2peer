47773ef2a82e1ffbff9e349e32bdb46c6eaa247e
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-05-08 14:35:16 +0200

    Switch to AWS database + coordinates formated for leaflet

--- api/app/models/User.model.js
+++ api/app/models/User.model.js
@@ -57,7 +57,7 @@ module.exports = class User {
 
         // use extern API to find coordinates
         console.log(process.env.APIKEY)
-        const api = await Wreck.get(`https://geocode.search.hereapi.com/v1/geocode?q=${country}+${city}&apiKey='${process.env.APIKEY}'`, {
+        const api = await Wreck.get(`https://geocode.search.hereapi.com/v1/geocode?q=${country}+${city}&apiKey=${process.env.APIKEY}`, {
             json: true
         });
         
