b50c22df6be9555f6f77fe76d3460e5babef541b
AuroreLeblois <leblois-aurore@hotmail.fr>
2020-05-09 12:24:45 +0200

    add a few infos in home page+ changed favicon

--- web/src/components/Home/index.js
+++ web/src/components/Home/index.js
@@ -3,18 +3,20 @@ import React from 'react';
 import { Container, Heading } from 'react-bulma-components';
 
 
-// == Import
-
 // == Composant
 const Home = () => {
   return (
     <Container>
       <Heading>
-        Hero title Primary
+        Peer2Pair
       </Heading>
       <Heading subtitle size={3}>
-        Subtitle
+        Le site de pair-programming entre pairs
       </Heading>
+     <p>Ici vous pourrez rechercher des développeurs plus expérimentés pour coder avec vous,
+       ou des élèves si vous souhaitez partager votre savoir...</p>
+      <p> Rentrez votre niveau, dites-nous si vous souhaitez coder en remote ou en personne.
+       Et c'est parti!</p> 
     </Container>
   );
 };
