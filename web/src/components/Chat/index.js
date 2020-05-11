0427a0ecccc463505b360803d7f45fccc34daed1
xooma <bec.maximilien@gmail.com>
2020-05-11 10:40:33 +0200

    fix passwordconfirm

--- web/src/components/Chat/index.js
+++ web/src/components/Chat/index.js
@@ -1,39 +1,39 @@
-// == Import npm
-import React, { useState } from 'react';
-import { Launcher } from 'react-chat-window';
+// // == Import npm
+// import React, { useState } from 'react';
+// import { Launcher } from 'react-chat-window';
 
-// == Import css
+// // == Import css
 
-const Chat = () => {
-  const [messageList, setMessageList] = useState([]);
+// const Chat = () => {
+//   const [messageList, setMessageList] = useState([]);
 
-  const onMessageWasSent = (message) => {
-    setMessageList([...messageList, message]);
-  };
+//   const onMessageWasSent = (message) => {
+//     setMessageList([...messageList, message]);
+//   };
 
-  const sendMessage = (text) => {
-    if (text.length > 0) {
-      setMessageList([...messageList, {
-        author: 'them',
-        type: 'text',
-        data: { text },
-      }]);
-    }
-  };
+//   const sendMessage = (text) => {
+//     if (text.length > 0) {
+//       setMessageList([...messageList, {
+//         author: 'them',
+//         type: 'text',
+//         data: { text },
+//       }]);
+//     }
+//   };
 
-  return (
-    <>
-      <Launcher
-        agentProfile={{
-          teamName: 'react-chat-window',
-          imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
-        }}
-        onMessageWasSent={onMessageWasSent}
-        messageList={messageList}
-        showEmoji
-      />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Launcher
+//         agentProfile={{
+//           teamName: 'react-chat-window',
+//           imageUrl: 'https://a.slack-edge.com/66f9/img/avatars-teams/ava_0001-34.png'
+//         }}
+//         onMessageWasSent={onMessageWasSent}
+//         messageList={messageList}
+//         showEmoji
+//       />
+//     </>
+//   );
+// };
 
-export default Chat;
+// export default Chat;
