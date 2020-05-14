aa56ee97c889c264a7ee28aa9f4e9a04ebb3b4a2
CharlesPhonepraseuth <charles.phonepraseuth@gmail.com>
2020-05-14 17:03:29 +0200

    Secured cookie password

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ const db = require('./app/models/db');
     server.auth.strategy('base', 'cookie', {
         cookie: {
             name: 'cookie',
-            password : 'Td2sXhE4Eghk8MBA3X96hgMqd66k8r2P',
+            password : process.env.COOKIEPASSWORD,
             isSecure: false,
             ttl: 1000*60*60*24 // 24 hours
         },
