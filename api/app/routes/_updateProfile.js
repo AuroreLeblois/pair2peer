e15736f56db3535d397bf2b6dc0f7e4c3d3135e8
AuroreLeblois <leblois-aurore@hotmail.fr>
2020-05-18 17:53:26 +0200

    errolist for front ok

--- api/app/routes/_updateProfile.js
+++ api/app/routes/_updateProfile.js
@@ -38,7 +38,7 @@ module.exports = {
         server.route({
             method: 'PATCH',
             path: '/update/profile',
-            options: {
+            config: {
                 auth: {
                     strategy: 'base',
                     mode: 'required',
@@ -59,15 +59,49 @@ module.exports = {
                         facebook_link: Joi.string().allow(''),
                         github_link: Joi.string().allow(''),
                         picture:Joi.string()
-                    })
-                },
+                    }),
+                    options: {
+                        // false mean I go through each key (payload) even if one error appears	
+                        // true mean the code throw directly an error starting from the first error and doesn't check other key
+                        abortEarly: false
+                    },
+                
+              
+                failAction: (request, h, err) => {
+                        
+                    // errors object will be the object who contains all the error messages (in french)
+                    const errors = {};
+                    const details = err.details;
+                    console.log(err.details)
+                    // depend on each error, it will write a specific error message
+                    let path = details[0].path[0];
+                    let typeError = details[0].type;
+                        // no need to write a specific error message if the input is empty because the constraint is set on the front side
+                        if (path === 'validatePassword' && typeError === 'any.only') {
+                            errors[path] = 'Le mot de passe et la confirmation ne correspondent pas';
+                        }
+                        if(path=== 'city'&& typeError=== 'string.empty'){
+                            errors[path]= 'La ville doit être définie'
+                        }
+                        if(path=== 'city'&& typeError=== 'string.empty'){
+                            errors[path]= 'La ville doit être définie'
+                        }
+                        if(path=== 'country'&& typeError=== 'string.empty'){
+                            errors[path]= 'Le pays doit être défini'
+                        }
+                    
+                
+                err.output.payload.message = errors;
+                        throw err;
+            },
+        },
                 description: 'handle update user profile',
                 tags: ['api', 'profile', 'validation']
             },
+        
             handler: async (request, h) => {
                 //grosse fonction update en perspective=> tout ce dont on a besoin
                 //un tableau d'erreurs vide
-                let error= [];
                 //le cookie email pour retrouver l'user
                 const email= request.state.cookie.email;
                 //on retrouve l'user de suite pour ne pas avoir à le refaire plus tard
@@ -93,6 +127,12 @@ module.exports = {
                 
                 //on compare le mdp avec la validation si mdp changé
                 //déjà, est ce que le user a rentré un mdp?
+                const errorList = {
+                    statusCode: 400,
+                    error: 'Bad Request',
+                    message: {}
+                };
+
                 if(password.length>0){
                     //est ce que le mdp fait bien 8 caractères au moins?
                     if(password.length>=8){
@@ -104,10 +144,10 @@ module.exports = {
                                             WHERE "id"=$2`,[hashPassword, userID]);
                         }
                         else{
-                            error.push('La validation et le mot de passe sont différents.');
+                            errorList.message.errorPassword='La validation et le mot de passe sont différents.';
                         }
                     }
-                    else{error.push(' Votre mot de passe doit faire 8 caractères minimum!')};
+                    else{ errorList.message.errorPassword=' Votre mot de passe doit faire 8 caractères minimum!'};
                 };
                //le pseudo si on désire le changer
                 if(pseudo.length>0 && pseudo!==result.rows[0].pseudo){
@@ -118,14 +158,14 @@ module.exports = {
                     }
                     //si le pseudo existe=> on le dit à l'utilisateur
                     else{
-                        error.push('user name already in use');
+                        errorList.message.pseudo="Nom d'utilisateur déjà pris";
                     }
                 };
                 
                 //si l'user change son détail=> usr_detail
                 //avant on vérifie que les input "required" sont conformes
                 if(city!==null&& country!==null && remote!==null
-                    ||city!== undefined&& country!== undefined&& remote!== undefined){
+                  ||city!== undefined&& country!== undefined&& remote!== undefined){
 
                         const detailExist= await db.query(`SELECT * FROM usr_detail WHERE usr_id=$1`,[userID]);
                         //il n'existe pas=> on insert
@@ -171,6 +211,23 @@ module.exports = {
                                                 longitude,userID]);
 
                             }
+                            if(city===null||city===undefined){
+                                errorList.message.city=`Il vous faut définir votre ville`;
+                            }
+                            if(country===null||country===undefined){
+                               errorList.message.country=`Merci de définir votre pays`;
+                            }
+                            if(remote===null||remote===undefined){
+                               errorList.message.remote=`Merci de nous dire si vous souhaitez travailler en remote`;
+                            }
+                            if (errorList.message.errorEmail
+                                ||errorList.message.errorPassword
+                                ||errorList.message.city
+                                ||errorList.message.country
+                                ||errorList.message.remote
+                                ||errorList.message.pseudo) {
+                                return errorList;
+                            }
                               
                              await db.query(`UPDATE usr_detail
                                             SET "remote"=$1,
@@ -188,36 +245,16 @@ module.exports = {
                                         };
                     
                     }
-                     
-                
-            // sinon on dit ce qu'il manque à l'utilisateur
-                else{
-                    if(city===null||city===undefined){
-                        error.push(`Il vous faut définir votre ville`);
-                    }
-                    if(country===null||country===undefined){
-                        console.log(`pays non défini`)
-                        error.push(`Merci de définir votre pays`);
-                    }
-                    if(remote===null||remote===undefined){
-                        console.log(`remote non défini`)
-                        error.push(`Merci de nous dire si vous souhaitez travailler en remote`);
-                    }
-                }
-    
-                        if(error.length>0){
-                            console.log(error)
-                            return h.response(error).code(400);
-                        }
-                        else{
+                   
                             const newResult = await db.query(`SELECT * FROM usr WHERE "id" = $1` ,[userID]);
                             const newProfile= await db.query(`SELECT * FROM usr_profile WHERE pseudo=$1 `,[newResult.rows[0].pseudo]);
                             // const newPlace= await db.query(`SELECT * FROM usr_map WHERE pseudo=$1`, [newResult.rows[0].pseudo]);
                             // const newPl= newPlace.rows[0];
                             const newPro= newProfile.rows[0];
                             return  {newPro};
-                        }
+                        
                     }
+                
                 });
 
                 server.route({
@@ -231,7 +268,6 @@ module.exports = {
                         },
                         validate: {
                             payload: Joi.object({
-                               
                                 language: Joi.string(),
                               
                             }),
